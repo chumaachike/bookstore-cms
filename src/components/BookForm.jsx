1cf4769b8e2544775db9337efb4e4cf298c07c48
Achike Chuma Edward <50702933+chumaachike@users.noreply.github.com>
2022-07-16 15:12:56 -0700

    styled the bookstore app

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuid } from 'uuid';
 import { addBooks } from '../redux/books/books';
+import styles from './Book.module.css';
 
 function BooksContainer() {
   const [author, addAuthor] = useState('');
@@ -27,12 +28,14 @@ function BooksContainer() {
     }
   };
   return (
-    <form onSubmit={submitBook}>
-      <h3>ADD NEW BOOK</h3>
-      <input type="text" placeholder="Book Title" value={title} onChange={changeTitle} />
-      <input type="text" placeholder="Author" value={author} onChange={changeAuthor} />
-      <button type="submit">Button</button>
-    </form>
+    <div className={styles.tyt}>
+      <h3 className={styles.formTitle}>ADD NEW BOOK</h3>
+      <form className={styles.formContainer} onSubmit={submitBook}>
+        <input type="text" className={styles.inputTextBox} placeholder="Book Title" value={title} onChange={changeTitle} />
+        <input type="text" className={styles.inputTextBox} placeholder="Author" value={author} onChange={changeAuthor} />
+        <button className={styles.addBookButton} type="submit">Button</button>
+      </form>
+    </div>
   );
 }
 
